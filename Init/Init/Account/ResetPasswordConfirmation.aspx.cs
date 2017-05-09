efa40324c354c2254d991c01b264a03687e568e9
Sarun <Sarun@DESKTOP-114S9O2>
2017-05-10 00:28:48 +0700

    Init begin

---
+++ Init/Init/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Init.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Init/Init/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Init.Global" Language="C#" %>
